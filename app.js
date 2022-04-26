62f85c732d36b6d27cb63af55618d3d8631b4ba4
Omar <ofernandomm@gmail.com>
2022-04-26 15:19:03 -0500

    Primer avance de MercadoLiebre

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('./public'))
+
+app.listen(3000, ()=> {
+    console.log('Servidor corriendo en puerto 3000')
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
