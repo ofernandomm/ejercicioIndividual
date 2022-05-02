6b4d1fa3913948aecd078dc517d1d898160bbd58
Omar <ofernandomm@gmail.com>
2022-05-01 23:20:02 -0500

    terminando formularios

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ app.get('/', (req,res)=>{
 app.get('/register', (req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
 })
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
+
+app.get('/home', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
